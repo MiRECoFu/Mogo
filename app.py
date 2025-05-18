7f6fb7d22a592f489323bd09cf2d937bae2fc112
1027066037@qq.com <1027066037@qq.com>
2025-05-18 13:54:45 +0800

    update

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
-
+OSS_ACCESS_KEY_ID = 'LTAI5t73baaMm8aAsK4Sr3iq'
+OSS_ACCESS_KEY_SECRET = 'oTD8yuieSavyN5vUW5YjVggX8gFE08'
 OSS_ENDPOINT = 'oss-cn-beijing.aliyuncs.com'  # 比如：oss-cn-hangzhou.aliyuncs.com
 OSS_BUCKET_NAME = 'mogo-bvh'
 
